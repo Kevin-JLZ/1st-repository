7034abdc7964b5973493553dbb584942881a1095
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-03-18 08:24:41 -0500

    Add files via upload

---
+++ 2nd/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Tipos de datos basicos
+    /*
+        Para trabajar con numeros enteros se utilizan los tipos
+        - short
+        - int
+        - long
+        - long long
+
+        Para declarar una variable Tipo Nombre_variable
+    */
+
+    int val; //Declarando una variable de tipo entero llamada val
+    val = 10;
+    cout << "El valor de val es: " << val << endl;
+    return 0;
+}
