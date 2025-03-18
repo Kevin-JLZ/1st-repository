e5560ed9992b96052184ef6022b73aa22799c93d
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-03-18 08:10:05 -0500

    Add files via upload

---
+++ 1st project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Welcome to Ccomp 1" << endl;
+    int a = 21;
+    cout << "a = " << a << endl;
+    return 0;
+}
