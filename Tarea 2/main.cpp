a07c6d7930256fe70f7f4474bfa96cb2f0a82c27
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-04-02 08:24:28 -0500

    Update main.cpp

--- Tarea 2/main.cpp
+++ Tarea 2/main.cpp
@@ -10,24 +10,45 @@ int main()
     getline(cin, cadena);
 
     cout << "Ingrese la palabra que desee encontrar en la cadena: " << endl;
-    char caracter;
-    cin >> caracter;
+    string palabra;
+    cin >> palabra;
 
     int len = cadena.length();
+    int len_palabra = palabra.length();
+    cout << len << " y " << len_palabra << endl;
     int i{0};
+    int j{0};
+
+    int a{0};
 
     int contador{0};
 
     while(i < len)
     {
-        if(cadena.at(i) == caracter){
-            contador++;
+        if(cadena.at(i) == palabra.at(0)){
+                while(j < len_palabra)
+                {
+                    if(cadena.at(i+j) == palabra.at(j)){
+                        a++;
+                        if(a == len_palabra){
+                            contador++;
+                        }
+                    }
+                    j++;
+                }
+                //cout << "j = " << j << endl;
+                j = 0;
+                a = 0;
+                //cout << "comprobacion: " << i << endl;
         }
         i++;
     }
 
-    if(contador > 0){
-        cout << "El caracter se encontro: " << contador << endl;
+    if(contador == 1){
+        cout << "La palabra se encontro: " << contador << " vez" << endl;
+    }
+    else if(contador >= 1){
+        cout << "La palabra se encontro: " << contador << " veces" << endl;
     }
     else{
         cout << "No se encontro el caracter en la cadena" << endl;
@@ -35,3 +56,4 @@ int main()
 
     return 0;
 }
+
