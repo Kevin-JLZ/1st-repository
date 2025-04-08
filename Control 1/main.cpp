44784b1eea095c8c1e026c9171ac70e3fa6b908c
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-04-08 08:01:27 -0500

    Add files via upload

---
+++ Control 1/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num{0};
+    cin >> num;
+
+    int cantidad = (num*2) - 1;
+
+    char a = '*';
+
+    cout << endl;
+    for(int i = 1; i <= num; i++)
+    {
+        for(int esp = 0; esp < num-i; esp++)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j < i; j++)
+        {
+            cout << a << ' ';
+        }
+        cout << endl;
+    }
+
+    for(int i = num-1; i > 0; i--)
+    {
+        for(int esp = 0; esp < num-i; esp++)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j < i; j++)
+        {
+            cout << a << ' ';
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
