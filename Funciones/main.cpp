3c9d56c2770ca39d4f8a3b8e4085caed6c0c9c8f
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-04-11 10:58:10 -0500

    Add files via upload

---
+++ Funciones/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool eval_prim(int n)
+{
+    int contador{0};
+    for(int i = 1; i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+            contador++;
+        }
+    }
+    if(contador == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool eval_mult(int numero, int primo)
+{
+    if(primo%numero == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int main()
+{
+    int numero{0};
+    cout << "Ingrese un numero: " << endl;
+    cin >> numero;
+
+    for(int primo = 0; primo < 20; )
+    {
+        bool comp_prim = eval_prim;
+        if(comp_prim == true)
+        {
+            bool comp_mult = eval_mult;
+            if(comp_mult == true)
+            {
+                cout << numero << " es multiplo de: " << primo << endl;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
