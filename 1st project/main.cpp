65f271a2748afa2249c623bc9771236bc62841b1
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-04-16 08:22:25 -0500

    Add files via upload

--- 1st project/main.cpp
+++ 1st project/main.cpp
@@ -1,12 +1,97 @@
 #include <iostream>
+#include <array>
+#include <vector>
 
 using namespace std;
 
+
+void imp_arr(int arr[], int size);
+/*
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "Welcome to Ccomp 1" << endl;
-    int a = 21;
-    cout << "a = " << a << endl;
+    //Implemente un programa que invierta los elementos de un arreglo de enteros,
+
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    imp_arr(arr, size);
+
+    int ini = 0;
+    int fin = size-1;
+
+    while(ini < fin){
+        int temp = arr[ini];
+        arr[ini] = arr[fin];
+        arr[fin] = temp;
+        ini++;
+        fin--;
+    }
+
+    imp_arr(arr, size);
+
     return 0;
 }
+*/
+
+/*
+void bubblesort(int arr[], int size)
+{
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size-i-1; j ++){ //size-1 tambien funciona
+            if(arr[j] > arr[j+1]){
+                int max = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = max;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    //Ordenar un array
+    int arr[] = {5, 3, 2, 8, 7, 1, 4, 6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    imp_arr(arr, size);
+
+    bubblesort(arr, size);
+
+    imp_arr(arr, size);
+
+
+    return 0;
+}
+
+
+void imp_arr(int arr[], int size)
+{
+    cout << "[";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
+    return;
+}
+
+*/
+
+void printArray(array<int, 5>)
+{
+    cout << "[";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
+    return;
+}
+int main()
+{
+    array<int, 5> arra = {1, 2, 3, 4, 5};
+    imp_arr(arra);
+
+    return 0;
+}
+
+
+
