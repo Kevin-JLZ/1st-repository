b9e7e174ad16fe0a393e0e148fa2e405e30322be
Kevin-JLZ <kevin.larico@ucsp.edu.pe>
2025-05-02 01:42:32 -0500

    Update main.cpp

--- Proyectos/Product_of_M/main.cpp
+++ Proyectos/Product_of_M/main.cpp
@@ -2,58 +2,103 @@
 
 using namespace std;
 
-void printArray(int height, int width, int **matriz)
-{
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
-            cout << *(*(matriz+j)+i) << " ";
+void printMatrix(int** matrix, int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cout << *(*(matrix+i)+j) << " ";
         }
         cout << endl;
+    }
+    cout << endl;
+}
+
+void fillMatrix(int** matrix, int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cin >> *(*(matrix + i) + j);
+        }
+    }
+    cout << endl;
+}
+
+int** createMatrix(int height, int width) {
+    int** matrix = new int* [height];
+    for (int i = 0; i < height; i++) {
+        *(matrix+i) = new int[width];
+    }
+    return matrix;
+}
+
+void deleteMatrix(int** matrix, int height) {
+    for (int i = 0; i < height; i++) {
+        delete[] matrix[i]; 
+    }
+    delete[] matrix;
+}
+
+void productMatrix(int** C, int** A, int** B, int A_height, int B_width) {
+    for (int i = 0; i < A_height; i++) {
+        for (int j = 0; j < B_width; j++) {
+            int temp = 0;
+            for (int k = 0; k < A_height; k++) {
+                temp += *(*(A + i) + k) * *(*(B + k) + j);
+            }
+            *(*(C + i) + j) = temp;
+        }
     }
 }
 
 int main()
 {
-    //Variables
     int A_height{0};
     int A_width{0};
-    cout << "Type the size of matriz A: " << endl;
+    cout << "Type the size of matrix A: " << endl;
     cout << "Height: ";
     cin >> A_height;
     cout << "Width: ";
     cin >> A_width;
-    int A[A_height][A_width];
-    for(int i = 0; i < A_height; i++){
-        for(int j = 0; j < A_width; j++){
-            cin >> *(*(A+j)+i);
-        }
-    }
-    for(int i = 0; i < A_height; i++){
-        for(int j = 0; j < A_width; j++){
-            cout << *(*(A+j)+i) << " ";
-        }
-        cout << endl;
-    }
-    printArray(A_height, A_width, A)
 
+    //Reservando espacio
+    int** A = createMatrix(A_height, A_width);
 
+    //Llenando valores
+    fillMatrix(A, A_height, A_width);
 
+    //Mostrar valores
+    printMatrix(A, A_height, A_width);
 
-/*
-    int B_height{0};
-    int B_width{0};
-    cout << "Type the size of matriz B: " << endl;
+    int B_height{ 0 };
+    int B_width{ 0 };
+    cout << "Type the size of matrix B: " << endl;
     cout << "Height: ";
     cin >> B_height;
     cout << "Width: ";
     cin >> B_width;
-    int B[B_height][B_width];
-    for(int i = 0; i < B_height; i++){
-        for(int j = 0; j < B_width; j++){
-            cin >> B[i][j];
-        }
-    }
-*/
+
+    //Reservando espacio
+    int** B = createMatrix(B_height, B_width);
+
+    //Llenando valores
+    fillMatrix(B, B_height, B_width);
+
+    //Mostrar valores
+    printMatrix(B, B_height, B_width);
+
+
+    //Creando nueva matriz
+    int** C = createMatrix(A_height, B_width);
+    
+    //Multiplicando Matrices A y B
+    cout << "The product of A and B is:   " << endl;
+    productMatrix(C, A, B, A_height, B_width);
+
+    printMatrix(C, A_height, B_width);
+
+
+    //Liberando memoria
+        deleteMatrix(A, A_height);
+        deleteMatrix(B, B_height);
+        deleteMatrix(C, A_height);
 
     return 0;
 }
